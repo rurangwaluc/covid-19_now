93336196e403b15944c91c11f925a33a9d69c716
rurangwa <rurangwajl7@gmail.com>
2020-04-16 10:18:22 +0200

    Update deployment 2

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -14,12 +14,15 @@ const Navbar =({ history }) => (
       <div>
          <nav className="navbar navbar-expand-lg ">
             <div className="container-fluid">
+            <div className='head-nav'>
+
                 <h2 className='logo'>STAY HOME - SAVE LIVES</h2>
                 <button className="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                     aria-label="Toggle navigation">
                     <i className="fas fa-align-justify"></i>
                 </button>
+            </div>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="nav navbar-nav ml-auto pr-5">
